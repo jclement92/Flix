7ecb3607d762f8544afb7976db3015a26a9f04ee
jclement92 <jclement92@yahoo.com>
2021-01-26 21:29:02 -0500

    Implement movie detail activity

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,28 +3,33 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String backdropPath;
     String posterPath;
     String title;
     String overview;
-    double average;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
-        average = Double.parseDouble(jsonObject.getString("vote_average"));
+        voteAverage = Double.parseDouble(jsonObject.getString("vote_average"));
     }
 
-    public double getAverage() {
-        return average;
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 
     // The correct way to implement this is to
@@ -55,7 +60,7 @@ public class Movie {
 
             // If rating is greater than 5, add the backdrop path
             // Else, add the movie
-            if (movie.getAverage() > 5) {
+            if (movie.getVoteAverage() > 5) {
                 movies.add(movie.getBackdropPath());
             } else {
                 movies.add(movie);
